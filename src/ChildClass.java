b3d3fbf7dac85a6d56a0c17fe08da117c22479c0
Sajan Singh <sajan.singh@nagarro.com>
2022-12-26 16:40:03 +0530

    first commit

---
+++ src/ChildClass.java
@@ -0,0 +1,8 @@
+
+public class ChildClass extends ParentClass{
+	public static void main(String[] args) {
+		display();
+		System.out.println("In child class");
+	}
+
+}
---
+++ src/ParentClass.java
@@ -0,0 +1,8 @@
+
+public class ParentClass {
+	static int a=10;
+	public static void display() {
+		System.out.println("In parent class");
+		System.out.println("Value of a= "+a);
+	}
+}
